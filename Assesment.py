74cda4bb3266f61300ccd3f4bb59bc4bc7edb176
James-Menzies9 <Jamesmenzies@cromwell.school.nz>
2025-03-20 13:25:50 +1300

    Add files via upload
    Put the random function in a couple question need to add loops

--- Assesment.py
+++ Assesment.py
@@ -21,8 +21,8 @@ print("There is a total of 8 question you have to answer")
 e.msgbox("Question 1")
 print("Question 1")
 print("Current score",Score)
-Answer1 = 100
-Question1 = e.integerbox("What is 100x12/12","Question 1")
+Answer1 = 99
+Question1 = e.integerbox("What is 100x12/12-1","Question 1")
 print(Question1)
 if Question1 == Answer1:
     Score +=1
@@ -32,45 +32,63 @@ else:
 e.msgbox("New score",Score)
 print(Score)
 
-random = random.randint (1,10)
-Answer2 = random
+Answer2 = random.randint (1,10)
 e.msgbox("Question 2")
 print("Question 2")
 Question2 = e.integerbox("Pick a number between 1 and 10","Question 2")
+print(Answer2)
 if Question2 == Answer2:
     Score +=1
     e.msgbox("Correct")
-    print("correct")
+    print("Correct")
 else:
     e.msgbox("Incorrect")
-    print("incorrect")
-e.msgbox("New score",Score)
-print(Score)
-#e.msgbox("Question 3")
-#print("Question 3")
+    print("Incorrect")
 
 e.msgbox("New score",Score)
 print(Score)
+
+Answer3 = str(random.randint(1,50))
+wrong = str(random.randint(1,50))
+wrong2 = str(random.randint(1,50))
+e.msgbox("Question 3")
+print("Question 3")
+Question3 = e.buttonbox("Pick the right number","Question3",choices=[wrong,wrong2,Answer3])
+
+if Question3 == Answer3:
+     Score +=1
+     e.msgbox("Correct")
+     print("Correct")
+else:
+    e.msgbox("Incorrect")
+    print("Incorrect")
+    print(Score)
+
+
 #e.msgbox("Question 4")
 #print("Question 4")
 
 e.msgbox("New score",Score)
 print(Score)
+
 #e.msgbox("Question 5")
 #print("Question 5")
 
 e.msgbox("New score",Score)
 print(Score)
+
 #e.msgbox("Question 6")
 #print("Question 6")
 
 e.msgbox("New score",Score)
 print(Score)
+
 #e.msgbox("Question 7")
 #print("Question 7")
 
 e.msgbox("New score",Score)
 print(Score)
+
 #e.msgbox("Question 8")
 #print("Question 8")
 
@@ -79,4 +97,7 @@ print(Score)
 
 if Score == 8:
     e.msgbox("Well done you got all the question correct")
-elif score < 4 and > 8:
+elif score < 4:
+    e.msgbox("Good try, try to get a better score.")
+else:
+    e.msgbox("Good job you got over half correct")
