2215d36a01be1075ccbd05d5fe3ed9aead3ef55c
James-Menzies9 <Jamesmenzies@cromwell.school.nz>
2025-05-15 13:31:07 +1200

    Add files via upload

--- Assesment.py
+++ Assesment.py
@@ -19,21 +19,20 @@ if 15 <= player_age <= 18:
 else:
     #keeps you in an endless loop
     while True:
-        e.msgbox("You can't play this game")           
-    
+        e.msgbox("You can't play this game")
+#Answer for question in list form
+answers=[99,random.randint (1,10),str(random.randint(1,50)),str("2/9/1945") and str("2 September 1945"),"Elon Musk","automatable manufacturer","The land","4pm"]      
 #keeps them in the loop until all the answers are correct
 while True:
         score = 0
         e.msgbox("Question 1")
         print("Question 1")
         print(score)
-        #Answer for question
-        Answer1 = 99
         #Askes question
         Question1 = e.integerbox("What is 100x12/12-1","Question 1")
         print(Question1)
         #says if your right or wrong and if you get correct you get a point (same for every question)
-        if Question1 == Answer1:
+        if Question1 == answer[0]:
             score +=1
         else:
             e.msgbox("Incorrect")
@@ -41,13 +40,12 @@ while True:
         e.msgbox(score,Score)
         print(score)
 
-        Answer2 = random.randint (1,10)
-        print(Answer2)
+        
         e.msgbox("Question 2")
         print("Question 2")
         Question2 = e.integerbox("Pick a number between 1 and 10","Question 2")
         print(Answer2)
-        if Question2 == Answer2:
+        if Question2 == answer[1]:
             score +=1
             e.msgbox("Correct")
             print("Correct")
@@ -58,14 +56,14 @@ while True:
         e.msgbox(score,Score)
         print(score)
 
-        Answer3 = str(random.randint(1,50))
+        
         wrong = str(random.randint(1,50))
         wrong2 = str(random.randint(1,50))
         e.msgbox("Question 3")
         print("Question 3")
         Question3 = e.buttonbox("Pick the right number","Question3",choices=[wrong,wrong2,Answer3])
 
-        if Question3 == Answer3:
+        if Question3 == answer[2]:
             score +=1
             e.msgbox("Correct")
             print("Correct")
@@ -74,13 +72,12 @@ while True:
             print("Incorrect")
             print(score)
         e.msgbox( score)
-        #Turns number into strings so theres no math calculation
-        Answer4 = str("2/9/1945") and str("2 September 1945")
+        #Turns number into strings so theres no math calculation 
         e.msgbox("Question 4")
         print("Question 4")
 
         Question4 = e.enterbox("When did World War 2 official end")
-        if str(Question4) == Answer4:
+        if str(Question4) == answer[3]:
             e.msgbox("Correct")
             print("Correct")
             score +=1
@@ -92,12 +89,11 @@ while True:
         e.msgbox(score,Score)
         print(score)
 
-        Answer5 ="Elon Musk"
         e.msgbox("Question 5")
         print("Question 5")
 
         Question5 = e.enterbox("Who owns SpaceX")
-        if Question5 == Answer5:
+        if Question5 == answer[4]:
             e.msgbox("Correct")
             print("Correct")
             score +=1
@@ -108,12 +104,11 @@ while True:
         e.msgbox(score,Score)
         print(score)
 
-        Answer6 ="automatable manufacturer"
         e.msgbox("Question 6")
         print("Question 6")
 
         Question6 = e.buttonbox("What is a BMW","Question 6",choices=("Pickup truck","automatable manufacturer","True supercar"))
-        if Question6 == Answer6:
+        if Question6 == answer[5]:
             e.msgbox("Correct")
             print("Correct")
             score +=1
@@ -124,13 +119,12 @@ while True:
         e.msgbox(score,Score)
         print(score)
 
-        Answer7 ="The land"
         e.msgbox("Question 7")
         print("Question 7")
 
         Question7 = e.buttonbox("Where does Mcdonalds gets most of its networth from","Question 7",choices=("The land","The food","Sponsers"))
 
-        if Question7 == Answer7:
+        if Question7 == answer[6]:
             e.msgbox("Correct")
             print("Correct")
             score +=1
@@ -141,7 +135,6 @@ while True:
         e.msgbox(score,Score)
         print(score)
 
-        Answer8 ="4pm"
         e.msgbox("Question 8")
         print("Question 8")
 
@@ -149,7 +142,7 @@ while True:
         e.msgbox(score,Score)
         print(score)
 
-        if Question8 == Answer8:
+        if Question8 == answer[7]:
             e.msgbox("Correct")
             print("Correct")
             score +=1
